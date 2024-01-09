7f477db3474521f17669921f24f905cc5fbb7ebd
Denis Carlos de Farias <denis_carlos_farias@hotmail.com>
2024-01-09 01:24:34 -0300

    Add project files.

---
+++ Buffer.cs
@@ -0,0 +1,19 @@
+﻿using CircularBuffer;
+
+namespace RingBuffer
+{
+    public class Buffer<T>
+    {
+        public CircularBuffer<T> CreateBuffer(int capacity, T[]? itens = null)
+        {
+            if (itens is not null)
+            return new CircularBuffer<T>(capacity, itens);
+
+            return new CircularBuffer<T>(capacity);
+        }
+        public void PrintBuffer(CircularBuffer<T> buffer)
+        {
+            Console.WriteLine($"{{{string.Join(",", buffer.ToArray())}}}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using RingBuffer;
+
+var buffer = new Buffer<int>();
+var ringBuuffer = buffer.CreateBuffer(5, [0,1,2]);
+
+Console.WriteLine("Initial buffer {0 , 1, 2}:");
+buffer.PrintBuffer(ringBuuffer);
+
+
+for (int i = 3; i < 7; i++)
+{
+    ringBuuffer.PushBack(i);
+}
+Console.WriteLine("\nAfter adding a 7 elements to a 5 elements capacity buffer:");
+buffer.PrintBuffer(ringBuuffer);
+
+
+ringBuuffer.PopFront();
+Console.WriteLine("\nbuffer.PopFront():");
+buffer.PrintBuffer(ringBuuffer);
+
+
+ringBuuffer.PopBack();
+Console.WriteLine("\nbuffer.PopBack():");
+buffer.PrintBuffer(ringBuuffer);
+
+for (int i = 2; i >= 0; i--)
+{
+    ringBuuffer.PushFront(i);
+}
+Console.WriteLine("\nbuffer.PushFront() {2,1,0} respectively:");
+buffer.PrintBuffer(ringBuuffer);
+
+ringBuuffer.Clear();
+Console.WriteLine("\nbuffer.Clear():");
+buffer.PrintBuffer(ringBuuffer);
+        
+
+
